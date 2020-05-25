580aa674cec0fb6f29db406dd1844d4acc412a50
charleseduardome <charleseduardo.me@gmail.com>
2020-05-25 19:14:16 -0300

    Express rate limit

---
+++ backend/src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+
+import redis from 'redis';
+import AppError from '@shared/errors/AppError';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.PASS || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'middleware',
+  points: 5,
+  duration: 1,
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch {
+    throw new AppError('Too many request', 429);
+  }
+}
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import { errors } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -15,8 +16,10 @@ import '@shared/container';
 
 const app = express();
 app.use(cors());
+app.use(rateLimiter);
 
 app.use(express.json());
+
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
