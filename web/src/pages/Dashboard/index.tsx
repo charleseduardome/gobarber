83d56ff710fc4d58e7b1ad1668bf5a52e055eee6
charleseduardome <charleseduardo.me@gmail.com>
2020-05-29 09:52:26 -0300

    Próximo agendamento

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Container, Header, HearderContent, Profile } from './styles';
-import { FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HearderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HearderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/53223042?s=460&u=778bc049c56c8c69dca834fd674b87740db2d9ab&v=4"
+                alt="Charles Eduardo"
+              />
+
+              <strong>Charles Eduardo</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
