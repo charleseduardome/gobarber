3e2ae19cb89418267e5ddb7c39f351a5a3bf7da9
charleseduardome <charleseduardo.me@gmail.com>
2020-06-01 16:24:26 -0300

    Página de perfil

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -21,6 +21,7 @@ import { FiPower, FiClock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 interface MonthAvailabilityItem {
   day: number;
@@ -145,8 +146,10 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
 
             <div>
-              <span>Bem-vindo</span>
-              <strong>{user.name}</strong>
+              <span>Bem-vindo,</span>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
